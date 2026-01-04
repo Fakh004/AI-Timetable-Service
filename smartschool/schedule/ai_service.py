4c33dda26d931ebc2d27aa026d86ee23a09ba2ca
fakh <fakhridin008@gmail.com>
2026-01-04 19:30:47 +0300

    fix ai-service

--- smartschool/schedule/ai_service.py
+++ smartschool/schedule/ai_service.py
@@ -13,6 +13,7 @@ class ScheduleAIService:
 
     def get_current_state(self):
         """Получаем текущее состояние расписания"""
+        
         teachers = list(Teacher.objects.all().values('id', 'name', 'subject', 'email'))
         rooms = list(Room.objects.all().values('id', 'number', 'capacity').annotate(type=F('room_type')))
         lessons = list(Lesson.objects.all().values(
